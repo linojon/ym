cab4e0cc0df1949894bb7e45234d62bc8d0aee53
Jonathan Linowes <jlinowes@m2s.com>
2011-09-11 00:16:23 -0400

    added compass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Ym::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false #true #for herokku #default false
+  config.serve_static_assets = false #true #for heroku #default false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
